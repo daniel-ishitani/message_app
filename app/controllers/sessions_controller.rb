941d91f14e75a70c2cd0d861b5b71514a3a8fc5f
daniel-ishitani <ishitanidim@gmail.com>
2019-08-11 21:46:40 +0000

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
     def new
 
     end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id]= user.id
+            flash[:success] = "You've successfully logged in"
+            redirect_to root_path
+        else
+            flash[:error] = "Wrong username or password"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "Successfully logged out"
+        redirect_to login_path
+    end
 end
